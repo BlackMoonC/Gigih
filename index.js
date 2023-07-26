e0c4de014e27f4e54c4549868cf63644bc76e838
Rakhel Cakra Kusumadinata Sera <rakelsera2001@gmail.com>
2023-07-26 18:15:40 +0700

    route product

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
 require("dotenv").config();
-
 const express = require("express");
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const mongoString = process.env.DATABASE_URL;
-console.log(mongoString);
-// mongoose.connect("mongodb://localhost:27017/spotify");
 
+// List routes
+const productRoute = require("./routes/product.route");
+
+// Mongoose configuration
 mongoose.connect(mongoString);
 const database = mongoose.connection;
 
-database.on('error', (error) => {
-    console.log(error);
-})
+database.on("error", (error) => {
+  console.log(error);
+});
 
-database.once('connected', () => {
-    console.log('connected');
-})
+database.once("connected", () => {
+  console.log("connected");
+});
 
 const app = express();
-
 app.use(express.json());
 
+// Setup body parser
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.use("/products", productRoute);
+
 app.listen(3000, () => {
-  console.log(`Server listening on ${3000}`);
+  console.log(`Server listening on http://localhost:${3000}`);
 });
---
+++ models/product.model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  link_product: {
+    required: true,
+    type: "string",
+  },
+  title: {
+    required: true,
+    type: "string",
+  },
+  price: {
+    required: true,
+    type: "string",
+  },
+});
+
+module.exports = mongoose.model("Product", productSchema);
